c05be1e0cfab53f8545d1c897696b4c1529fc726
David Chea <chea.david.pro@gmail.com>
2019-12-31 00:17:50 +0100

    Add controllers in storyboard

---
+++ LeBaluchon/Support/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  LeBaluchon
+//
+//  Created by David Chea on 30/12/2019.
+//  Copyright © 2019 David Chea. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {}
---
+++ LeBaluchon/Support/SceneDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  LeBaluchon
+//
+//  Created by David Chea on 30/12/2019.
+//  Copyright © 2019 David Chea. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    // MARK: - Property
+    
+    var window: UIWindow?
+}
